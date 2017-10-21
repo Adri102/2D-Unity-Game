b98c5ca736a563335926d9413591278d44e6e579
Adri102 <adri.arranz102@gmail.com>
2017-10-21 13:02:54 +0200

    Added the project

---
+++ Assets/Editor/CollisionDetectorEditor.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CollisionDetector))]
+public class CollisionDetectorEditor : Editor
+{
+    static bool showBase;
+    static bool showProperties = true;
+
+    public override void OnInspectorGUI()
+    {
+        CollisionDetector newCollision = (CollisionDetector)target;
+
+        showProperties = EditorGUILayout.Foldout(showProperties, "Properties", true, EditorStyles.toolbarDropDown);
+
+        GUIStyle myStyle = EditorStyles.boldLabel;
+        if(newCollision.isGrounded) myStyle.normal.textColor = Color.green;
+        else myStyle.normal.textColor = Color.red;
+        EditorGUILayout.LabelField("IsGrounded", myStyle);
+
+        GUIStyle myStyle2 = EditorStyles.boldLabel;
+        if(newCollision.wasGroundedLastFrame) myStyle2.normal.textColor = Color.green;
+        else myStyle2.normal.textColor = Color.red;
+        EditorGUILayout.LabelField("WasGroundedLastFrame", myStyle2);
+
+        GUIStyle myStyle3 = EditorStyles.boldLabel;
+        if(newCollision.justGrounded) myStyle3.normal.textColor = Color.green;
+        else myStyle3.normal.textColor = Color.red;
+        EditorGUILayout.LabelField("JustGrounded", myStyle3);
+
+        GUIStyle myStyle4 = EditorStyles.boldLabel;
+        if(newCollision.justNotGrounded) myStyle4.normal.textColor = Color.green;
+        else myStyle4.normal.textColor = Color.red;
+        EditorGUILayout.LabelField("JustNotGrounded", myStyle4);
+
+        GUIStyle myStyle5 = EditorStyles.boldLabel;
+        if(newCollision.isFalling) myStyle5.normal.textColor = Color.green;
+        else myStyle4.normal.textColor = Color.red;
+        EditorGUILayout.LabelField("IsFalling", myStyle5);
+
+
+        GUIStyle myStyle6 = EditorStyles.boldLabel;
+        if(newCollision.isWalled) myStyle6.normal.textColor = Color.green;
+        else myStyle6.normal.textColor = Color.red;
+        EditorGUILayout.LabelField("IsWalled", myStyle6);
+
+        GUIStyle myStyle7 = EditorStyles.boldLabel;
+        if(newCollision.wasWalledLastFrame) myStyle7.normal.textColor = Color.green;
+        else myStyle7.normal.textColor = Color.red;
+        EditorGUILayout.LabelField("WasWalledLastFrame", myStyle7);
+
+        GUIStyle myStyle8 = EditorStyles.boldLabel;
+        if(newCollision.justWalled) myStyle8.normal.textColor = Color.green;
+        else myStyle8.normal.textColor = Color.red;
+        EditorGUILayout.LabelField("JustWall", myStyle8);
+
+        GUIStyle myStyle9 = EditorStyles.boldLabel;
+        if(newCollision.justNotWalled) myStyle9.normal.textColor = Color.green;
+        else myStyle9.normal.textColor = Color.red;
+        EditorGUILayout.LabelField("JustNotWalled", myStyle9);
+
+        GUIStyle myStyle10 = EditorStyles.boldLabel;
+        if(newCollision.isCeiling) myStyle10.normal.textColor = Color.green;
+        else myStyle10.normal.textColor = Color.red;
+        EditorGUILayout.LabelField("isCeiling", myStyle10);
+
+        GUIStyle myStyle11 = EditorStyles.boldLabel;
+        if(newCollision.wasCeilingLastFrame) myStyle11.normal.textColor = Color.green;
+        else myStyle11.normal.textColor = Color.red;
+        EditorGUILayout.LabelField("WasCeilingLastFrame", myStyle11);
+
+        GUIStyle myStyle12 = EditorStyles.boldLabel;
+        if(newCollision.justCeiled) myStyle12.normal.textColor = Color.green;
+        else myStyle12.normal.textColor = Color.red;
+        EditorGUILayout.LabelField("JusCeiled", myStyle12);
+
+        GUIStyle myStyle13 = EditorStyles.boldLabel;
+        if(newCollision.justNotCeiled) myStyle13.normal.textColor = Color.green;
+        else myStyle13.normal.textColor = Color.red;
+        EditorGUILayout.LabelField("JustNotCeiled", myStyle13);
+
+        showBase = EditorGUILayout.Foldout(showBase, "BaseInspector", true, EditorStyles.toolbarDropDown);
+        if(showBase) base.OnInspectorGUI();
+    }
+
+}
---
+++ Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour {
+
+    [Header("Ground")]
+    [Header("State")]
+    public bool isGrounded;
+    public bool wasGroundedLastFrame;
+    public bool justGrounded;
+    public bool justNotGrounded;
+    public bool isFalling;
+
+    [Header("Filter")]
+    public ContactFilter2D filterGround;
+    public int maxCollidersGround = 1;
+    public bool checkGround = true;
+
+    [Header("Box Properties")]
+    public Vector2 groundBoxPos;
+    public Vector2 groundBoxSize;
+
+    [Header("Wall")]
+    [Header("State")]
+    public bool isWalled;
+    public bool wasWalledLastFrame;
+    public bool justWalled;
+    public bool justNotWalled;
+    
+
+    [Header("Filter")]
+    public ContactFilter2D filterWall;
+    public int maxCollidersWall = 1;
+    public bool checkWall = true;
+
+    [Header("Box Properties")]
+    public Vector2 wallBoxPos;
+    public Vector2 wallBoxSize;
+
+    [Header("Ceiling")]
+    [Header("State")]
+    public bool isCeiling;
+    public bool wasCeilingLastFrame;
+    public bool justCeiled;
+    public bool justNotCeiled;
+
+
+    [Header("Filter")]
+    public ContactFilter2D filterCeiling;
+    public int maxCollidersCeiling = 1;
+    public bool checkCeiling = true;
+
+    [Header("Box Properties")]
+    public Vector2 ceilingBoxPos;
+    public Vector2 ceilingBoxSize;
+
+
+
+    private void FixedUpdate()
+    {
+        ResetState();
+        GroundDetection();
+        WallDetection();
+        CeilingDetection();
+    }
+
+    void ResetState()
+    {
+        wasGroundedLastFrame = isGrounded;
+        isFalling = !isGrounded;
+        isGrounded = false;
+        justGrounded = false;
+        justNotGrounded = false;
+
+        wasWalledLastFrame = isWalled;
+        isWalled = false;
+        justWalled = false;
+        justNotWalled = false;
+
+        wasCeilingLastFrame = isCeiling;
+        isCeiling = false;
+        justCeiled = false;
+        justNotCeiled = false;
+    }
+    void GroundDetection()
+    {
+        if(!checkGround) return;
+
+        Vector3 pos = this.transform.position + (Vector3)groundBoxPos;
+        Collider2D[] results = new Collider2D[maxCollidersGround];
+
+        int numColliders = Physics2D.OverlapBox(pos, groundBoxSize, 0, filterGround, results);
+
+        if(numColliders > 0 )
+        {
+            isGrounded = true;
+        }
+
+        if(!wasGroundedLastFrame && isGrounded) justGrounded = true;
+        if(wasGroundedLastFrame && !isGrounded) justNotGrounded = true;
+    }
+
+    void WallDetection()
+    {
+        if (!checkWall) return;
+
+        Vector3 pos = this.transform.position + (Vector3)wallBoxPos;
+        Collider2D[] results = new Collider2D[maxCollidersWall];
+
+        int numColliders = Physics2D.OverlapBox(pos, wallBoxSize, 0, filterWall, results);
+
+        if (numColliders > 0)
+        {
+            isWalled = true;
+        }
+
+        if (!wasWalledLastFrame && isWalled) justWalled = true;
+        if (wasWalledLastFrame && !isWalled) justNotWalled = true;
+    }
+    void CeilingDetection()
+    {
+        if (!checkCeiling) return;
+
+        Vector3 pos = this.transform.position + (Vector3)ceilingBoxPos;
+        Collider2D[] results = new Collider2D[maxCollidersCeiling];
+
+        int numColliders = Physics2D.OverlapBox(pos, ceilingBoxSize, 0, filterCeiling, results);
+
+        if (numColliders > 0)
+        {
+            isCeiling = true;
+        }
+
+        if (!wasCeilingLastFrame && isCeiling) justCeiled = true;
+        if (wasCeilingLastFrame && !isCeiling) justNotCeiled = true;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Vector3 posGround = this.transform.position + (Vector3)groundBoxPos;
+        Gizmos.DrawWireCube(posGround, groundBoxSize);
+
+        Gizmos.color = Color.blue;
+        Vector3 posWall = this.transform.position + (Vector3)wallBoxPos;
+        Gizmos.DrawWireCube(posWall, wallBoxSize);
+
+        Gizmos.color = Color.yellow;
+        Vector3 posCeiling = this.transform.position + (Vector3)ceilingBoxPos;
+        Gizmos.DrawWireCube(posCeiling, ceilingBoxSize);
+    }
+}
