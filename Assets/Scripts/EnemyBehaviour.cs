a7c62d57e0d25e18cf97f221c5cfe7dbc493fbbe
CristianLuzon <cristianluzon@gmail.com>
2018-03-03 00:26:24 +0100

    Added background
    Same script as the ground.
    Deleted unknown script.

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour {
 
-    public Transform tilemapGround1;
+    public Transform Sprite;
     public int speed;
     public int minRandom;
     public int maxRandom;
     int random;
 
-	// Update is called once per frame
-	void Update ()
+    public void Start()
     {
-        tilemapGround1.Translate(Vector2.left * speed * Time.deltaTime);
+        Sprite = transform;
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+        Sprite.Translate(Vector2.left * speed * Time.deltaTime);
         random = Random.Range(minRandom, maxRandom);
-        if (tilemapGround1.position.x < -8) tilemapGround1.position = new Vector3(random, tilemapGround1.position.y, tilemapGround1.position.z);
+        if (Sprite.position.x < -8) Sprite.position = new Vector3(random, Sprite.position.y, Sprite.position.z);
     }
 }
